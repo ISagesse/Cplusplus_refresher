bd848783d3380ccc410ac5b88b7fba5fc3d7b825
Israel Sagesse <israelsagesse@gmail.com>
2022-02-19 12:37:22 -0500

    adding a transfer feature, only between acounts

--- miscellaneous/atm/Source.cpp
+++ miscellaneous/atm/Source.cpp
@@ -3,69 +3,67 @@
 using namespace std;
 
 // using functions and a while loop to create an ATM machine
-// check balance
+// check balances
 // money deposit
 // money withdraw
 // show menu
 
 void show_menu() {
 	cout << "\n********** Menu **********" << endl;
-	cout << "1. Check Balance" << endl;
+	cout << "1. Check checking_balance" << endl;
 	cout << "2. Deposit" << endl;
 	cout << "3. Withdraw" << endl;
-	cout << "4. EXIT" << endl;
+	cout << "4. Transfer" << endl;
+	cout << "5. EXIT" << endl;
 	cout << "************************ \n" << endl;
 }
 
-double check_balance(double balance) {
-	return balance;
-}
-
-double deposit(double balance) {
+double deposit(double checking_balance) {
 	double amount;
-	double new_balance;
+	double new_checking_balance;
 
 	cout << "********** Deposit **********" << endl;
 	cout << "Enter the amount: ";
 	cin >> amount;
 	if (amount <= 0) {
 		cout << "Invalid amount" << endl;
-		new_balance = balance;
+		new_checking_balance = checking_balance;
 	}
 	else {
 		cout << "Money has been successfully added." << endl;
-		new_balance = balance + amount;
+		new_checking_balance = checking_balance + amount;
 	}
 	cout << "************************" << endl;
 
-	return new_balance;
+	return new_checking_balance;
 }
 
-double withdraw(double balance) {
+double withdraw(double checking_balance) {
 	double amount;
-	double new_balance;
+	double new_checking_balance;
 
 	cout << "********** Withdraw **********" << endl;
 	cout << "Enter the amount: ";
 	cin >> amount;
-	if (amount - balance <= 0) {
+	if (amount - checking_balance <= 0) {
 		cout << "Insufficient funds or Invalid amount" << endl;
-		new_balance = balance;
+		new_checking_balance = checking_balance;
 	}
 	else {
 		cout << "Money has been successfully withdraw." << endl;
-		new_balance = balance - amount;
+		new_checking_balance = checking_balance - amount;
 	}
 	cout << "************************" << endl;
 
-	return new_balance;
+	return new_checking_balance;
 }
 
 int main() {
 
 	bool end = false;
 	int option;
-	double balance = 500;
+	double checking_balance = 500;
+	double saving_balance = 500;
 
 	while (end != true) {
 
@@ -76,17 +74,49 @@ int main() {
 
 		switch (option) {
 		case 1:
-			cout << "********** Balance **********" << endl;
-			cout << "You have $" << check_balance(balance) << " available." << endl;
+			cout << "********** Balance available **********" << endl;
+			cout << "Checking balance: $" << checking_balance << endl;
+			cout << "Saving balance: $" << saving_balance << endl;
 			cout << "************************" << endl;
 			break;
 		case 2:
-			balance = deposit(balance);
+			checking_balance = deposit(checking_balance);
 			break;
 		case 3:
-			balance = withdraw(balance);
+			checking_balance = withdraw(checking_balance);
 			break;
 		case 4:
+			cout << "********** Transfer **********" << endl;
+			int opt;
+			double amount;
+			cout << "1. Transfer funds form saving to checking." << endl;
+			cout << "2. Transfer funds form checking to saving." << endl;
+			cout << "How can I help you: ";
+			cin >> opt;
+			if (opt == 1) {
+				cout << "Enter amount: ";
+				cin >> amount;
+				if (amount > 0) {
+					checking_balance += amount;
+					saving_balance -= amount;
+					cout << "Successfuly transfer to checking" << endl;
+				}
+			}
+			else if (opt == 2) {
+				cout << "Enter amount: ";
+				cin >> amount;
+				if (amount > 0) {
+					checking_balance -= amount;
+					saving_balance += amount;
+					cout << "Successfuly transfer to saving" << endl;
+				}
+			}
+			else {
+				cout << "Invalid!!" << endl;
+			}
+			cout << "************************" << endl;
+			break;
+		case 5:
 			cout << "********** Exit **********" << endl;
 			cout << "Thank you! and Good bye." << endl;
 			cout << "************************" << endl;
