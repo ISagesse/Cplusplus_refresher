b3f7758737087483c792e77c278499a75f1538a1
Israel Sagesse <israelsagesse@gmail.com>
2022-04-04 05:37:41 -0400

    Dynamic array with pointer

---
+++ day_five/dynamic_array/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//Dynamic array with pointer.
+
+int main() {
+	int size;
+	cout << "Array size: ";
+	cin >> size;
+	//int myArray[size];
+	int* myArray = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Array [" << i << "]: ";
+		cin >> myArray[i];
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < size; i++) {
+		cout << myArray[i] << " ";
+		//cout << *(myArray + i) << " "; another way to get the array value without indexing.
+	}
+
+	return 0;
+}
