f3089617681b8eebedb513d09856c0c1e233db53
Israel Sagesse <israelsagesse@gmail.com>
2022-04-13 10:13:21 -0400

    use auto

--- day eight/vector_declaration/Source.cpp
+++ day eight/vector_declaration/Source.cpp
@@ -11,6 +11,7 @@ int main() {
 	// inicialize a new vector with a size of n which is 3 and x as the value which is 10.
 
 	for (auto i = v.begin(); i != v.end(); i++) {
+		// use auto to avoid initialize a vector with vector<int>::iterator
 		cout << (*i) << " ";
 		// to access those array element we have to use a pointer in front of i to return the value.
 	}
